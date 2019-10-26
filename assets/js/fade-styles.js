8d40c195328f0bd2699b15853f0c07c90ef7fbf4
physics4life <m@physics4.life>
2019-10-26 12:23:20 -0400

    20191026-update
    changed js to wait for dom content loading

--- assets/js/fade-styles.js
+++ assets/js/fade-styles.js
@@ -1,27 +1,28 @@
-// check which elements need to be faded in
-var fadeInElements = document.body.querySelectorAll(".fade-in");
+document.addEventListener('DOMContentLoaded',function(){
+	// check which elements need to be faded in
+	var fadeInElements = document.body.querySelectorAll(".fade-in");
 
-// determine whether the element has entered or is above the viewport
-var enteredViewport = function(elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (bounding.top <= (window.innerHeight || document.documentElement.clientHeight));
-}
+	// determine whether the element has entered or is above the viewport
+	var enteredViewport = function(elem) {
+		var bounding = elem.getBoundingClientRect();
+		return (bounding.top <= (window.innerHeight || document.documentElement.clientHeight));
+	}
 
-// fade in header and any other elements in the viewport
-window.onload = function() {
-    document.body.querySelector("header").classList.remove("fade");
-    for (var i = 0; i < fadeInElements.length; i++) {
-        if (enteredViewport(fadeInElements[i])) {
-            fadeInElements[i].classList.remove("fade");
-        }
-    }
-}
+	// fade in any elements in or above the viewport
+	window.onload = function() {
+		for (var i = 0; i < fadeInElements.length; i++) {
+			if (enteredViewport(fadeInElements[i])) {
+				fadeInElements[i].classList.remove("fade");
+			}
+		}
+	}
 
-// fade in elements as they become visible in the viewport
-window.addEventListener('scroll', function(){
-    for (var i = 0; i < fadeInElements.length; i++) {
-        if (enteredViewport(fadeInElements[i])) {
-            fadeInElements[i].classList.remove("fade");
-        }
-    }
+	// fade in elements as they become visible in the viewport
+	window.addEventListener('scroll', function(){
+		for (var i = 0; i < fadeInElements.length; i++) {
+			if (enteredViewport(fadeInElements[i])) {
+				fadeInElements[i].classList.remove("fade");
+			}
+		}
+	});
 });
