a8f8a77e33e2e8fa34cbef98e2e162d0f239fd7f
physics4life <m@physics4.life>
2019-10-30 19:23:43 -0400

    2019/10/30-logo-update

--- assets/js/fade-styles.js
+++ assets/js/fade-styles.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded',function(){
 	// check which elements need to be faded in
 	var fadeInElements = document.body.querySelectorAll(".fade-in");
-
+		
 	// determine whether the element has entered or is above the viewport
 	var enteredViewport = function(elem) {
 		var bounding = elem.getBoundingClientRect();
 		return (bounding.top <= (window.innerHeight || document.documentElement.clientHeight));
 	}
-
+	
 	// fade in any elements in or above the viewport
 	window.onload = function() {
 		for (var i = 0; i < fadeInElements.length; i++) {
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded',function(){
 			}
 		}
 	}
-
+	
 	// fade in elements as they become visible in the viewport
 	window.addEventListener('scroll', function(){
 		for (var i = 0; i < fadeInElements.length; i++) {
